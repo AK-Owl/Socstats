72a2826e76e1e0951af57d4d8b408aeb1bf5a7ba
AliceOwl <a.kvaratskheliia.18@abdn.ac.uk>
2019-10-11 14:56:20 +0100

    analysis controller created

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'grayscale/index'
   resources :freedoms
   get 'home/index'
-
+  get 'analysis/index'
+  
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
