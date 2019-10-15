f77d3769f8fcd7b2db5dacaa797b1095571ee18d
t17sk18 <s.karanam.18@abdn.ac.uk>
2019-10-15 12:00:51 +0100

    rendering forms for data analysis

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   resources :freedoms
   get 'home/index'
   get 'analysis/index'
-  
+  get 'analysis/show'
+  get 'analysis/dataanalysis'
   root 'home#index'
+
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
