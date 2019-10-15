6e1057d3b8f6fa7a13ea4a743f57005dd711884e
AliceOwl <a.kvaratskheliia.18@abdn.ac.uk>
2019-10-15 21:08:14 +0100

    bootstrap integration. Home page working, analysis throws an error: back up files before attempt to precompile

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,10 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
+#to enable the asset pipeline
+require "sprockets/railtie"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
