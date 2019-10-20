f01d871f545dd5348867d39b8570a4cacc4ee5ad
AliceOwl <a.kvaratskheliia.18@aberdeen.ac.uk>
2019-10-20 15:10:58 +0100

    fetched data wrapped with bootstrap

--- app/controllers/livedata_controller.rb
+++ app/controllers/livedata_controller.rb
@@ -2,7 +2,7 @@
 class LivedataController < ApplicationController
   def index
     # Searching The Guardian API with the following link
-    response = HTTParty.get('https://content.guardianapis.com/search?show-fields=bodyText&q=human%20AND%20(development%20OR%20freedom)&from-date=2014-01-01&api-key=test')
+    response = HTTParty.get('https://content.guardianapis.com/search?show-fields=bodyText&q=human%20AND%20(development%20OR%20freedom)&from-date=2014-01-01&api-key=4f724b82-90ef-4d6d-8ec5-054543388de5')
     # Guardian API KEY = 4f724b82-90ef-4d6d-8ec5-054543388de5
    
     # convert the result into JSON object
